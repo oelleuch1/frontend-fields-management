309951999f4342f0cb45389ec1587a645fb98b55
Elleuch <elleuchoussama4@gmail.com>
2024-03-10 23:45:01 +0100

    add review

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 v-html="msg"></h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <div :class="boxActive" class="card">
+    <button type="button" :disabled="count > 4" v-on:click="incrementCount">count is {{ count }}</button>
+    <div> {{ getLabel() }}</div>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
+
+  <div>{{ fullName }}</div>
+
+  <input placeholder="Name" v-model="count" />
 
   <p>
     Check out
@@ -21,14 +26,57 @@
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+  <ul>
+    <li v-for="(element, index) in items" >
+       {{ element }}
+    </li>
+  </ul>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'Hello world!',
-      count: 0
+      msg: '<ul> <li> Hello world! </li> <li> Vue Project</li> </ul>',
+      count: 0,
+      name: 'Emna',
+      lastName: 'DOGHMANE',
+      items: [
+          'Element 1',
+          'Element 12',
+          'Element 3',
+          'Element 55',
+      ]
+    }
+  },
+  computed: {
+    fullName() {
+      return this.name + ' ' + this.lastName
+    },
+    boxActive() {
+      return this.name === 'Emna' ? 'active' : 'inactive'
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+      this.name = 'Oussama'
+    },
+    getInput(event) {
+      event.preventDefault()
+      console.log(event.target.value)
+    },
+    getLabel() {
+      if (this.count < 3) {
+        return 'Under 3'
+      } else {
+       return 'Greater than 3'
+      }
+    },
+    setCount(event) {
+      this.count = event.target.value
     }
   }
 }
